956bddec38d5defe2840d5e5abd33f4368657e62
MichaelRozental <mr@mrr.co.il>
2026-08-30 11:17:08 +0300

    Deploy: Service Worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  אסטרטגיה: Cache-First לקבצים סטטיים, Network-First לשאר
 // ═══════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'rr-v65';
+const CACHE_NAME = 'rr-v66';
 
 // קבצים שיישמרו בcache בעת ההתקנה
 const STATIC_ASSETS = [
